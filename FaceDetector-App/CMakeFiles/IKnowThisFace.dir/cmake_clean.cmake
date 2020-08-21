d9542ed37d7a88c101bd7ff31109c5d23590279f
Yasin Tunçer <33193910+yasintuncer@users.noreply.github.com>
2020-08-21 23:39:56 +0300

    MTCNN Face Detector App

---
+++ FaceDetector-App/CMakeFiles/IKnowThisFace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IKnowThisFace.dir/src/main.cpp.o"
+  "IKnowThisFace"
+  "IKnowThisFace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IKnowThisFace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
